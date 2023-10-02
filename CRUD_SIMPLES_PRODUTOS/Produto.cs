044fec12bc050b6b3e88708343b18711bd777266
Lais Brito <65972314+laisbrito1@users.noreply.github.com>
2023-10-02 18:27:46 -0300

    Add files via upload

---
+++ CRUD_SIMPLES_PRODUTOS/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_SIMPLES_PRODUTOS
+{
+
+
+        public class Produto
+        {
+            public int ID { get; set; }
+            public string Nome { get; set; }
+
+            public Produto(int id, string nome)
+            {
+                ID = id;
+                Nome = nome;
+            }
+        }
+
+    
+}
